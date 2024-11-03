78a5880f154f12d22e9a296ac6c74737e64184e2
Ondrej Hulík <hulik.ondrej@gmail.com>
2024-11-03 16:39:28 +0100

    Working version

---
+++ stock_picker.rb
@@ -0,0 +1,19 @@
+def stock_picker(stocks)
+  highestYield = 0
+  bestStart = 0
+  bestSell = 0
+  stocks.each do |startingDay|
+     stocks.each do |sellingDay|
+        if startingDay - sellingDay < highestYield and stocks.index(startingDay) < stocks.index(sellingDay)
+          highestYield = startingDay - sellingDay
+          bestStart = startingDay
+          bestSell = sellingDay
+        end
+     end
+  end
+  # p [bestStart, bestSell]
+  # p [stocks.index(bestStart), stocks.index(bestSell)]
+  return [stocks.index(bestStart), stocks.index(bestSell)]
+end
+
+stock_picker([17,3,6,9,15,8,6,1,10])
