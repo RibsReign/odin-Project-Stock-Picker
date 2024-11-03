003fb38568ce9ff3c01fab305829e9ddd366500c
Ondrej Hulík <hulik.ondrej@gmail.com>
2024-11-03 16:42:07 +0100

    Add test for edge cases

--- stock_picker.rb
+++ stock_picker.rb
@@ -11,9 +11,10 @@ def stock_picker(stocks)
         end
      end
   end
-  # p [bestStart, bestSell]
-  # p [stocks.index(bestStart), stocks.index(bestSell)]
+  p [bestStart, bestSell]
+  p [stocks.index(bestStart), stocks.index(bestSell)]
   return [stocks.index(bestStart), stocks.index(bestSell)]
 end
 
-stock_picker([17,3,6,9,15,8,6,1,10])
+# stock_picker([17,3,6,9,15,8,6,1,10])
+# stock_picker([50,17,2,3,6,9,15,8,6,10,1])
